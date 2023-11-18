c4711918282fd31fa23b011218f6f5de1b07dcb6
AdamTaylor9 <tayloradamross@gmail.com>
2023-11-17 20:45:48 -0500

    city search functionality added

--- world.js
+++ world.js
@@ -1,25 +1,36 @@
 window.onload = function() {
     var lookupButton = document.getElementById('lookup');
+    var lookupCity = document.getElementById('citylookup');
     var input = document.getElementById('country');
     var result = document.getElementById('result');
     var rhttp;
+
     
     lookupButton.addEventListener("click", function(element){
         element.preventDefault();
     
         rhttp = new XMLHttpRequest();
-        var url = "http://localhost/info2180-lab5/world.php?country="+input.value;
+        var url = "http://localhost/info2180-lab5/world.php?country="+input.value+"&lookup=country";
         rhttp.onreadystatechange = loadList;
         rhttp.open('GET', url);
         rhttp.send();
     });
     
+
+    lookupCity.addEventListener("click", function(element){
+        element.preventDefault();
     
+        rhttp = new XMLHttpRequest();
+        var url = "http://localhost/info2180-lab5/world.php?country="+input.value+"&lookup=city";
+        rhttp.onreadystatechange = loadList;
+        rhttp.open('GET', url);
+        rhttp.send();
+    });
+
     function loadList(){
-        if (rhttp.readyState == XMLHttpRequest.DONE && rhttp.status == 200){
+        if (rhttp.readyState == 4 && rhttp.status == 200){
             var response = rhttp.responseText;
             result.innerHTML = response;
         }
     }
-    
 }
--- world.php
+++ world.php
@@ -10,18 +10,31 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
-$cQuery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $cQuery->fetchAll(PDO::FETCH_ASSOC);
+$lookup = filter_input(INPUT_GET, "lookup", FILTER_SANITIZE_STRING);
+
+$countryQuery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+if ($lookup=="country"){
+  $results = $countryQuery->fetchAll(PDO::FETCH_ASSOC);
+}
+else{
+  $citiesQuery = $conn->query("SELECT cities.* FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+  $results = $citiesQuery->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
 ?>
 
 <?php
+
 if (isset($country)){
-  echo("<table>");
+  if ($lookup=="country"){
+    echo("<table>");
       echo("<tr>");
-        echo("<th>Country Name</th>");
-        echo("<th>Continent</th>");
-        echo("<th>Independence Year</th>");
-        echo("<th>Head of State</th>");
+        echo("<th>COUNTRY</th>");
+        echo("<th>CONTINENT</th>");
+        echo("<th>YEAR OF INDEPENDENCE</th>");
+        echo("<th>HEAD OF STATE</th>");
       echo("</tr>");
     foreach ($results as $row){
       echo("<tr>");
@@ -31,6 +44,25 @@ if (isset($country)){
         echo("<td>".$row["head_of_state"]."</td>");
       echo("</tr>");
     }
+    echo("</table>");
+  }
+  else{
+    echo("<table>");
+      echo("<tr>");
+        echo("<th>CITY NAME</th>");
+        echo("<th>DISTRICT</th>");
+        echo("<th>POPULATION</th>");
+      echo("</tr>");
+    foreach ($results as $row){
+      echo("<tr>");
+        echo("<td>".$row["name"]."</td>");
+        echo("<td>".$row["district"]."</td>");
+        echo("<td>".$row["population"]."</td>");
+      echo("</tr>");
+    }
   echo("</table>");
+  }
 }
+
+
 ?>
